2f4dc2396b24ff9301307a7ef0e9d859e7499484
Stefan Akie <stefan.akie@grammm.com>
2020-11-26 15:07:46 +0100

    Remove password expiration and align aliases list with other tabs

--- src/containers/UserDetails.js
+++ src/containers/UserDetails.js
@@ -56,6 +56,16 @@ const styles = theme => ({
   select: {
     minWidth: 60,
   },
+  list: {
+    padding: 0,
+    marginTop: -8,
+  },
+  listItem: {
+    padding: theme.spacing(1, 0),
+  },
+  listTextfield: {
+    flex: 1,
+  },
 });
 
 class UserDetails extends PureComponent {
@@ -85,17 +95,6 @@ class UserDetails extends PureComponent {
     { name: 'Out of date', ID: 2 },
     { name: 'Deleted', ID: 3 },
   ]
-
-  expires = [
-    { name: '1 week', ID: 0 },
-    { name: '1 month', ID: 1 },
-    { name: '1 year', ID: 2 },
-    { name: '100 years', ID: 3 },
-    { name: 'Never', ID: 4 },
-  ]
-
-  timeZones = [-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, 1, 0,
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   async componentDidMount() {
     const { fetch, fetchRoles } = this.props;
@@ -275,20 +274,6 @@ class UserDetails extends PureComponent {
                 <TextField
                   select
                   className={classes.input}
-                  label={t("Password expiration time")}
-                  fullWidth
-                  value={user.expire || 0}
-                  onChange={this.handleInput('expire')}
-                >
-                  {this.expires.map((expire, key) => (
-                    <MenuItem key={key} value={expire.ID}>
-                      {expire.name}
-                    </MenuItem>
-                  ))}
-                </TextField>
-                <TextField
-                  select
-                  className={classes.input}
                   label={t("Status")}
                   fullWidth
                   value={user.addressStatus || 0}
@@ -325,31 +310,19 @@ class UserDetails extends PureComponent {
                 </TextField>
               </React.Fragment>}
               {tab === 1 && <React.Fragment>
-                <TextField
-                  select
-                  className={classes.input}
-                  label={t("Language")}
-                  fullWidth
-                  value={language || 0}
-                  onChange={this.handlePropertyChange('language')}
-                >
-                  <MenuItem value={0}>
-                    {t('english')}
-                  </MenuItem>
-                </TextField>
-                <TextField 
-                  className={classes.input}
-                  label={t("Job title")}
-                  fullWidth
-                  value={title || ''}
-                  onChange={this.handlePropertyChange('title')}
-                />
                 <TextField 
                   className={classes.input}
                   label={t("Display name")}
                   fullWidth
                   value={displayname || ''}
                   onChange={this.handlePropertyChange('displayname')}
+                />
+                <TextField 
+                  className={classes.input}
+                  label={t("Job title")}
+                  fullWidth
+                  value={title || ''}
+                  onChange={this.handlePropertyChange('title')}
                 />
                 <TextField 
                   className={classes.input} 
@@ -379,6 +352,18 @@ class UserDetails extends PureComponent {
                   value={postaladdress || ''}
                   onChange={this.handlePropertyChange('postaladdress')}
                 />
+                <TextField
+                  select
+                  className={classes.input}
+                  label={t("Language")}
+                  fullWidth
+                  value={language || 0}
+                  onChange={this.handlePropertyChange('language')}
+                >
+                  <MenuItem value={0}>
+                    {t('english')}
+                  </MenuItem>
+                </TextField>
                 <TextField 
                   className={classes.input} 
                   label={t("Comment")} 
@@ -413,9 +398,9 @@ class UserDetails extends PureComponent {
               </React.Fragment>}
               {tab === 3 && <React.Fragment>
                 <List className={classes.list}>
-                  {user.aliases.map((alias, idx) => <ListItem key={idx}>
+                  {user.aliases.map((alias, idx) => <ListItem key={idx} className={classes.listItem}>
                     <TextField
-                      style={{ flex: 1 }}
+                      className={classes.listTextfield}
                       value={alias}
                       label={'Alias ' + (idx + 1)}
                       onChange={this.handleAliasEdit(idx)}
