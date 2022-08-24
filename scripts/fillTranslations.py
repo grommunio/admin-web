c1ee4873927d7c45c811e58e8e8bfe1bc3edfc8e
Stefan Akie <stefan.akie@grommunio.com>
2022-08-24 14:02:55 +0200

    Add script to fill translation keys

---
+++ scripts/fillTranslations.py
@@ -0,0 +1,21 @@
+import json
+import os
+import sys
+
+# Opening JSON file
+i18nDir = sys.argv[1] or "../src/i18n/"
+files = os.listdir(i18nDir)
+
+if("de-DE.json" in files):
+    with open(i18nDir + 'de-DE.json') as deFile:
+        de = json.load(deFile)
+        for file in files:
+            with open(i18nDir + file) as uncompletedFile:
+                fileContent = json.load(uncompletedFile)
+                for key, value in de.items():
+                    if key in fileContent:
+                        continue
+                    else:
+                        fileContent[key] = ""
+            with open(i18nDir + file, 'w') as fp:
+                json.dump(fileContent, fp, indent=2, sort_keys=True, ensure_ascii=False)
