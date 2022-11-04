f2cbd07ddce6203a899048c56bb30fa8c896cc57
Stefan Akie <stefan.akie@grommunio.com>
2022-11-04 11:39:13 +0100

    Implement design toolbox

--- src/containers/License.js
+++ src/containers/License.js
@@ -4,21 +4,28 @@
 import React, { PureComponent } from 'react';
 import { withStyles } from '@mui/styles';
 import PropTypes from 'prop-types';
-import { Button, CircularProgress, Collapse, Grid, IconButton, List, ListItem, ListItemButton, ListItemText, Paper,
+import { Button, CircularProgress, Collapse, Dialog, DialogContent, DialogTitle, Grid, IconButton, List, ListItem, ListItemButton, ListItemText, Paper,
+  TextField,
+  Tooltip,
   Typography } from '@mui/material';
-import { withTranslation } from 'react-i18next';
+import { Trans, withTranslation } from 'react-i18next';
 import { uploadLicenseData } from '../actions/license';
 import { connect } from 'react-redux';
 import TableViewContainer from '../components/TableViewContainer';
 import { fetchDomainData } from '../actions/domains';
-import { ExpandLess, ExpandMore } from '@mui/icons-material';
+import { AddCircle, CopyAll, ExpandLess, ExpandMore } from '@mui/icons-material';
 import { fetchPlainUsersData, fetchUserCount } from '../actions/users';
 import moment from 'moment';
+import { addItem, copyToClipboard } from '../utils';
 
 const styles = theme => ({
   paper: {
     margin: theme.spacing(3, 2, 3, 2),
     padding: theme.spacing(2, 2, 2, 2),
+  },
+  design: {
+    margin: theme.spacing(3, 0),
+    padding: theme.spacing(2),
   },
   description: {
     display: 'inline-block',
@@ -37,6 +44,9 @@ const styles = theme => ({
   headline: {
     marginRight: 8,
   },
+  grid: {
+    margin: theme.spacing(0, 1),
+  },
   gridItem: {
     display: 'flex',
     flex: 1,
@@ -51,6 +61,28 @@ const styles = theme => ({
     flexDirection: 'column',
     margin: theme.spacing(0, 2),
   },
+  input: {
+    margin: theme.spacing(1),
+  },
+  flexBox: {
+    display: 'flex',
+  },
+  imgPreview: {
+    maxWidth: '100%',
+  },
+  imageGroup: {
+    margin: theme.spacing(0, 0, 4, 0),
+  },
+  jsonPreview: {
+    whiteSpace: 'pre',
+  },
+  pre: {
+    display: 'inline',
+    margin: theme.spacing(0, 0.5)
+  },
+  typography: {
+    margin: theme.spacing(1, 0),
+  },
 });
 
 class License extends PureComponent {
@@ -61,15 +93,20 @@ class License extends PureComponent {
     expandedDomainIdxs: [],
     domainUsers: {},
     counts: {},
+    customImages: [],
+    configOpen: false,
   }
 
   componentDidMount() {
     this.props.fetchDomains()
       .then(async () => {
-        const { Domains, fetchCount } = this.props;
+        const { Domains, fetchCount, customImages } = this.props;
         const counts = {};
         Domains.forEach(async domain => counts[domain.domainname] = await fetchCount(domain.ID));
-        this.setState({ counts });
+        this.setState({
+          counts,
+          customImages: Object.entries(customImages).map(([hostname, images]) => ({ hostname, ...images})),
+        });
       })
       .catch(snackbar => this.setState({ snackbar }));
   }
@@ -113,9 +150,35 @@ class License extends PureComponent {
 
   toggleDomainExpansion = () => this.setState({ domainsExpanded: !this.state.domainsExpanded });
 
+  handleAddImageGroup = () => this.setState({
+    customImages: addItem(this.state.customImages, {}),
+  });
+
+  handleImgInput = (field, idx) => e => {
+    const copy = [...this.state.customImages];
+    copy[idx][field] = e.target.value;
+    this.setState({ customImages: copy });
+  }
+
+  handleShowConfig = () => this.setState({ configOpen: true });
+
+  handleConfigClose = () => this.setState({ configOpen: false });
+
+  handleCopyToClipboard = () => {
+    copyToClipboard('"customImages": ' + JSON.stringify(
+      this.state.customImages.reduce((prevValue, currentValue) => ({
+        ...prevValue,
+        [currentValue.hostname]: {
+          ...currentValue,
+          hostname: undefined,
+        },
+      }), {}), null, 4)
+    )
+  }
+
   render() {
     const { classes, t, license, Domains } = this.props;
-    const { snackbar, expandedDomainIdxs, domainUsers, domainsExpanded, counts } = this.state;
+    const { snackbar, expandedDomainIdxs, domainUsers, domainsExpanded, counts, customImages, configOpen } = this.state;
 
     return (
       <TableViewContainer
@@ -210,6 +273,137 @@ class License extends PureComponent {
           />
         </Paper>
         <div className={classes.about}>
+          <Typography variant='h2' className={classes.licenseContainer}>
+            {t("Design")}
+          </Typography>
+          <Typography variant="caption" className={classes.subtitle}>
+            {t("design_sub")}
+          </Typography>
+          <Paper className={classes.design} elevation={1}>
+            {customImages.map(({ hostname, logo, logoLight, icon, background, backgroundDark}, idx) =>
+              <div className={classes.imageGroup} key={idx}>
+                <TextField
+                  label={t("Hostname")}
+                  value={hostname || ''}
+                  className={classes.input}
+                  required
+                  fullWidth
+                  onChange={this.handleImgInput("hostname", idx)}
+                />
+                <div className={classes.flexBox}>
+                  <Grid container direction="column" alignItems="center" className={classes.grid}>
+                    <TextField
+                      label={t("Logo")}
+                      value={logo || ''}
+                      className={classes.iconInput}
+                      variant="standard"
+                      onChange={this.handleImgInput("logo", idx)}
+                      fullWidth
+                    />
+                    <img src={logo || ''} alt="" className={classes.imgPreview}/>
+                  </Grid>
+                  <Grid container direction="column"  alignItems="center" className={classes.grid}>
+                    <TextField
+                      label={t("Logo light")}
+                      value={logoLight || ''}
+                      className={classes.iconInput}
+                      variant="standard"
+                      onChange={this.handleImgInput("logoLight", idx)}
+                      fullWidth
+                    />
+                    <img src={logoLight || ''} alt="" className={classes.imgPreview}/>
+                  </Grid>
+                  <Grid container direction="column"  alignItems="center" className={classes.grid}>
+                    <TextField
+                      label={t("Icon")}
+                      value={icon || ''}
+                      className={classes.iconInput}
+                      variant="standard"
+                      onChange={this.handleImgInput("icon", idx)}
+                      fullWidth
+                    />
+                    <img src={icon || ''} alt="" className={classes.imgPreview}/>
+                  </Grid>
+                  <Grid container direction="column" alignItems="center" className={classes.grid}>
+                    <TextField
+                      label={t("Background")}
+                      value={background || ''}
+                      className={classes.iconInput}
+                      variant="standard"
+                      onChange={this.handleImgInput("background", idx)}
+                      fullWidth
+                    />
+                    <img src={background || ''} alt="" className={classes.imgPreview}/>
+                  </Grid>
+                  <Grid container direction="column" alignItems="center" className={classes.grid}>
+                    <TextField
+                      label={t("Background dark")}
+                      value={backgroundDark || ''}
+                      className={classes.iconInput}
+                      variant="standard"
+                      onChange={this.handleImgInput("backgroundDark", idx)}
+                      fullWidth
+                    />
+                    <img src={backgroundDark || ''} alt="" className={classes.imgPreview}/>
+                  </Grid>
+                </div>
+              </div>
+            )}
+            <div className={classes.progressContainer}>
+              <Tooltip title={t("Add new set of icons for explicit hostname")}>
+                <IconButton onClick={this.handleAddImageGroup}>
+                  <AddCircle color="primary"/>
+                </IconButton>
+              </Tooltip>
+            </div>
+            <Grid container>
+              <Button variant='contained' onClick={this.handleShowConfig}>
+                {t("Show config")}
+              </Button>
+            </Grid>
+          </Paper>
+          <Dialog
+            maxWidth="lg"
+            fullWidth
+            onClose={this.handleConfigClose}
+            open={configOpen}
+          >
+            <DialogTitle>
+              {t("Serverconfig")}
+              <Tooltip placement="top" title={t('Copy config')}>
+                <IconButton onClick={this.handleCopyToClipboard} size="large">
+                  <CopyAll />
+                </IconButton>
+              </Tooltip>
+            </DialogTitle>
+            <DialogContent>
+              <pre>
+                <code className={classes.jsonPreview}>
+                  &quot;customImages&quot;: {JSON.stringify(customImages.reduce((prevValue, currentValue) => ({
+                    ...prevValue,
+                    [currentValue.hostname]: {
+                      ...currentValue,
+                      hostname: undefined,
+                    },
+                  }), {}), null, 4)}
+                </code>
+              </pre>
+              <Typography className={classes.typography}>
+                <Trans i18nKey="configInstructions1">
+                  Copy these lines into
+                  <pre className={classes.pre}>
+                    /etc/grommunio-admin-common/config.json
+                  </pre>
+                </Trans>.
+                <Trans i18nKey="configInstructions2">
+                  Be careful not to duplicate the
+                  <pre className={classes.pre}>&quot;customImages&quot;</pre> key
+                </Trans>.
+              </Typography>
+            </DialogContent>
+          </Dialog>
+        </div>
+        <div className={classes.about}>
           <Typography variant='h2' className={classes.licenseContainer}>About</Typography>
           <Typography variant="caption">
             grommunio is Copyright © 2020-{moment().year()}. All rights reserved.
@@ -242,13 +436,15 @@ License.propTypes = {
   fetchUsers: PropTypes.func.isRequired,
   fetchCount: PropTypes.func.isRequired,
   Domains: PropTypes.array.isRequired,
+  customImages: PropTypes.object,
 };
 
 const mapStateToProps = state => {
-  const { license, domains } = state;
+  const { license, domains, config } = state;
   return {
     license: license.License,
     Domains: domains.Domains,
+    customImages: config.customImages,
   };
 };
 
