d5b16067fae279eecb11179e674f3a4b735d28ad
Stefan Akie <stefan.akie@grommunio.com>
2026-03-24 17:00:59 +0100

    Improve SilentRefresh implementation

--- src/components/SilentRefresh.tsx
+++ src/components/SilentRefresh.tsx
@@ -1,29 +1,34 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2020-2026 grommunio GmbH
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { refreshToken } from "../actions/auth";
 import { useAppSelector, useAppDispatch } from "../store";
 
 
-const SilentRefresh = () => {
+const SilentRefresh = (): JSX.Element | null => {
   const config = useAppSelector((state) => state.config);
   const dispatch = useAppDispatch();
-  let fetchInterval = null;
+  const fetchInterval = useRef<ReturnType<typeof setInterval> | null>(null);
 
   useEffect(() => {
     refresh();
+
     return () => {
-      clearInterval(fetchInterval);
-    }
-  }, [])
+      if (fetchInterval.current) {
+        clearInterval(fetchInterval.current);
+      }
+    };
+  }, []);
 
   const refresh = () => {
-    fetchInterval = setInterval(() => {
+    fetchInterval.current = setInterval(() => {
       console.info("token refreshed");
-      dispatch(refreshToken()).catch(() => console.info("Failed to refresh token"));
-    }, 1000 * (config.tokenRefreshInterval || 60 * 60 * 24)); // Default: 24h
-  }
+      dispatch(refreshToken()).catch(() =>
+        console.info("Failed to refresh token")
+      );
+    }, 1000 * (config.tokenRefreshInterval || 60 * 60 * 24));
+  };
 
   return null;
 }
